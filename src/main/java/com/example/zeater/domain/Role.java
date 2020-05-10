5927ea39a487f50613da8f0bf9fc8b0670d2feb7
Zello <zellohello0@gmail.com>
2020-05-10 18:39:22 +0300

    have put security and change architecture of project

---
+++ src/main/java/com/example/zeater/domain/Role.java
@@ -0,0 +1,5 @@
+package com.example.zeater.domain;
+
+public enum Role { //enum - это перчисления
+    USER;
+}
---
+++ src/main/java/com/example/zeater/repos/UserRepo.java
@@ -0,0 +1,9 @@
+package com.example.zeater.repos;
+
+import com.example.zeater.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+
+}
